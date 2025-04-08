a96790d0c3e5b50d1c473a664152f86f482b1363
deine.kleine.liebe@gmail.com <ms.balashova.2001@gmail.com>
2025-04-08 22:47:54 +0300

    Work with files

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
 import BadRequestError from '../errors/bad-request-error'
-import { types } from '../middlewares/file'
+import { allowedExtentions, types } from '../middlewares/file'
 
 export const uploadFile = async (
     req: Request,
@@ -12,8 +12,21 @@ export const uploadFile = async (
         return next(new BadRequestError('Файл не загружен'))
     }
     try {
-        if (!req.file.mimetype || !types.includes(req.file.mimetype)) {
+        if (!req.file.mimetype || !types.includes(req.file.mimetype) ) {
             return next(new BadRequestError('Можно загружать только файлы в форматах: png, jpg, jpeg, gif и svg+xml'))
+        }
+        if (
+            !req.file.originalname || !extname(!req.file.originalname) || !allowedExtentions.includes(
+                extname(!req.file.originalname).toLowerCase()
+            )
+        ) {
+            return next(new BadRequestError('Недопустимый формат файла'));
+        }
+        if (
+            !req.headers['content-length'] ||
+            Number(req.headers['content-length']) <= 2000
+        ) {
+            return next(new BadRequestError('Слишком небольшой размер файла'));
         }
         const fileName = process.env.UPLOAD_PATH
             ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
@@ -28,3 +41,7 @@ export const uploadFile = async (
 }
 
 export default {}
+function extname(arg0: boolean) {
+    throw new Error('Function not implemented.')
+}
+
