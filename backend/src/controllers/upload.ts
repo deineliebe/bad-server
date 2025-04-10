ea52e1e65ea35e7319dc1b438ec7bcc765705022
deine.kleine.liebe@gmail.com <ms.balashova.2001@gmail.com>
2025-04-10 21:27:48 +0300

    Try to delete extra checks

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,30 +1,18 @@
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
-import BadRequestError from '../errors/bad-request-error'
-import file, { allowedExtensions, types } from '../middlewares/file'
 import { extname } from 'path'
-import { faker } from '@faker-js/faker'
+import BadRequestError from '../errors/bad-request-error'
+import { types } from '../middlewares/file'
 
 export const uploadFile = async (
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
-    if (!req.file || !req.file.path) {
+    if (!req.file) {
         return next(new BadRequestError('Файл не загружен'))
     }
     try {
-        if (!req.file?.mimetype || !types.includes(req.file.mimetype)) {
-            return next(
-                new BadRequestError(
-                    'Можно загружать только файлы в форматах: png, jpg, jpeg, gif и svg'
-                )
-            )
-        }
-        const fileExtension = extname(req?.file?.originalname).toLowerCase()
-        if (!fileExtension || !allowedExtensions.includes(fileExtension)) {
-            return next(new BadRequestError('Недопустимый формат файла'))
-        }
         if (req.file?.size < 2000) {
             return next(new BadRequestError('Слишком небольшой размер файла'))
         }
